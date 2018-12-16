8cc50cd8cff74f971827b08d058c8ea5a96a95aa
Gideon Kanyangela <gkanyangela@gmail.com>
2018-12-16 14:50:01 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Gideon Kanyangela on 2018/12/16.
+//  Copyright © 2018 Paperless Software Solution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
